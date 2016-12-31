9adb3e4f4621452feac1230dec3c1e305aef208e
matthew vinck <matthew.vinck@outlook.com>
2016-12-30 22:16:39 -0600

    Map page built

--- TrackThat/app/src/main/java/com/example/vinck/trackthat/MainPage.java
+++ TrackThat/app/src/main/java/com/example/vinck/trackthat/MainPage.java
@@ -1,7 +1,9 @@
 package com.example.vinck.trackthat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainPage extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class MainPage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+    }
+
+    public void Open_Map(View view){
+        Intent i = new Intent(getApplicationContext(),Map_Activity.class);
+        startActivity(i);
+
     }
 }
